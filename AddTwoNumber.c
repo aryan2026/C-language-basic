3f3e91af93acbb8b6120e6d9f13ececac0c857bd
aryan2026 <98771996+aryan2026@users.noreply.github.com>
2022-02-01 00:16:58 +0530

    Create AddTwoNumber.c

---
+++ AddTwoNumber.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,sum;
+    
+    printf("Enter the value of first number: ");
+    scanf("%d",&a);
+    printf("Enter the value of second number: ");
+    scanf("%d",&b);
+    
+    sum=a+b;
+    printf("The sum of first and second number is : %d",sum);
+
+    return 0;
+}
