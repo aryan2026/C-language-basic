9558394ff92a679d77ad8f427fabf736c8e1e664
aryan2026 <98771996+aryan2026@users.noreply.github.com>
2022-02-09 22:41:04 +0530

    Create Addition.c

---
+++ Addition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("Enter the first number :  ");
+    scanf("%d",&a);
+    printf("Enter the second number :");
+    scanf("%d",&b);
+    
+    sum=a+b;
+    printf("The sum of two numbers are : %d",sum);
+    
+    return 0;
+}
