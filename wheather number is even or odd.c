e83c8592804438af82e6d2a92be90de9009fbb9e
aryan2026 <98771996+aryan2026@users.noreply.github.com>
2022-03-23 11:02:04 +0530

    Create wheather number is even or odd.c

---
+++ wheather number is even or odd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("ENTER THE NUMBER :  ");
+    scanf("%d",&num);
+    
+    if(num%2==0)
+
+    {
+        printf("ITS AN EVEN NUMBER");
+    }
+    else
+    {
+        printf("ITS AN ODD NUMBER");
+    }
+    
+    return 0;
+}
